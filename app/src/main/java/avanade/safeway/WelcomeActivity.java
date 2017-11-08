09ae6afedde806f986fcc0e28b4604650175b21b
wkonda <wkonda0@gmail.com>
2017-11-08 11:16:54 +0100

    --

--- app/src/main/java/avanade/safeway/WelcomeActivity.java
+++ app/src/main/java/avanade/safeway/WelcomeActivity.java
@@ -1,11 +1,13 @@
 package avanade.safeway;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.CheckBox;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -43,7 +45,27 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         });
 
+        final Button[] buttons_profil = new Button[3];
+        buttons_profil[0] = (Button) findViewById(R.id.button_pedestrian);
+        buttons_profil[1] = (Button) findViewById(R.id.button_cyclist);
+        buttons_profil[2] = (Button) findViewById(R.id.button_driver);
+        View.OnClickListener button_listener = new View.OnClickListener() {
+            public void onClick(View v) {
+                int number = Integer.parseInt((String) v.getTag());
+                for (int i = 0; i < 3; ++i)
+                    if (i == number)
+                        buttons_profil[i].setBackgroundColor(Color.GREEN);
+                    else
+                        buttons_profil[i].setBackgroundColor(Color.RED);
+            }
+        };
+        buttons_profil[0].setOnClickListener(button_listener);
+        buttons_profil[1].setOnClickListener(button_listener);
+        buttons_profil[2].setOnClickListener(button_listener);
 
+    }
+
+    public class ButtonLineListener {
 
     }
 }
