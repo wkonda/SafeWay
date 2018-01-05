59f3ddcda1edb65504c2f5046c2ba8ab545b2cf9
wkonda <wkonda0@gmail.com>
2018-01-05 02:22:30 +0100

    broadcast is created

--- app/src/main/java/avanade/safeway/SafeWayGPSListener.java
+++ app/src/main/java/avanade/safeway/SafeWayGPSListener.java
@@ -1,22 +1,23 @@
 package avanade.safeway;
 
+import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-/**
- * Created by wkond on 08/11/2017.
- */
 
 ////////////GPS LISTENER
 public class SafeWayGPSListener implements android.location.LocationListener {
     private static final String TAG = "SafeWayGPSListener";
-    //Location mLastLocation;
-    private boolean position_available;
+    //private boolean position_available;
     private Location current_location;
+    private Context context;
 
-    public SafeWayGPSListener(String provider) {
-        Log.e(TAG, "LocationListener " + provider);
+    SafeWayGPSListener(String provider, Context context) {
+        Log.i(TAG, "LocationListener " + provider);
+        this.context = context;
         current_location = new Location(provider);
     }
 
@@ -24,10 +25,12 @@ public class SafeWayGPSListener implements android.location.LocationListener {
     public void onLocationChanged(Location location) {
         if (location != null) {
             current_location = location;
-            Log.e(TAG, "onLocationChanged: " + location);
+            Log.i(TAG, "onLocationChanged: " + location);
+            sendCurrentLocation();
         }
 
     }
+
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -37,21 +40,24 @@ public class SafeWayGPSListener implements android.location.LocationListener {
     @Override
     public void onProviderEnabled(String s) {
         Log.e(TAG, "onProviderEnabled: " + s);
-        position_available = true;
+        //position_available = true;
     }
 
     @Override
     public void onProviderDisabled(String s) {
         Log.e(TAG, "onProviderDisabled: " + s);
-        position_available = false;
+        //position_available = false;
     }
 
-    public boolean isPosition_available() {
-        return position_available;
-    }
+    //public boolean isPosition_available() { return position_available;}
 
-    public Location getLocation() {
-        return current_location;
+    //Location getLocation() {return current_location;}
+
+    private void sendCurrentLocation() {
+        Intent intent = new Intent("com.avanade.safeway.safe_way_gps_position");
+        intent.putExtra("location", current_location.toString());
+
+        LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
     }
 }
 ///////////////END GPS LISTENER
